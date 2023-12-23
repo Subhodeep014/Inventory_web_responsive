1eef40bf195f648100d167ec5a2a8faef1a1b67b
Subhodeep Basak <85038045+Subhodeep014@users.noreply.github.com>
2023-12-23 20:50:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const toggle = document.querySelector('.toggle');
 const sidebar = document.querySelector('.sidebar');
 const main = document.querySelector('.main');
+console.log(main);
 const sideIcon = document.querySelectorAll('.side-icon');
 const sideTitle = document.querySelectorAll('.side-title');
+const cardBox = document.querySelector(".cardBox");
+const addData = document.querySelector(".addData");
 
 toggle.onclick = function(){ 
     sidebar.classList.toggle('active');
     main.classList.toggle('active');
 }
 console.log(sideIcon,sideTitle);
-function changeColor(element){
+function changeColor(element, iconName){
     sideIcon.forEach(function(icon){
         icon.classList.remove('active');
     })
@@ -20,5 +23,12 @@ function changeColor(element){
     var title = element.querySelector('.title.side-title');
     icon.classList.add('active');
     title.classList.add('active');
-
+    if(iconName==='dashboard'){
+        main.style.display='block'
+        addData.style.display='none';
+    }
+    if(iconName==='inputData'){
+        addData.style.display='block';
+        main.style.display='none';
+    }
 }
